abb79d5df5c0035cda6445037af361df5c783be8
lgy <gylinuxer@163.com>
2016-03-12 18:42:24 +0800

    modified  and not finished

--- AVL.c
+++ AVL.c
@@ -11,9 +11,30 @@ struct node{
 };
 struct node * tree_root = NULL; 
 
+int max(int a,int b)
+{
+    return a>b?a:b;
+}
+int abs(int a,int b)
+{
+    return a>b?a-b:b-a;
+}
+int get_bf(struct node * node)
+{
+     if(!node)
+	return 0;
 
+     return (!node->left?0:node->left->maxheight)-(!node->right?0:node->right->maxheight);
+}
+int get_max_height(struct node * node)
+{
+     if(!node)
+	return 0;     
 
-
+     int h_left = node->left?0:node->left->maxheight;
+     int h_right= node->right?0:node->right->maxheight;
+     return h_left > h_right ? h_left : h_right; 
+} 
 void insert_avl(struct node * node)
 {
 	struct node * next;
@@ -21,7 +42,8 @@ void insert_avl(struct node * node)
 	struct node * min_no_bf_node;
 	struct node * min_rev_node = NULL;
 	int cur_dir = 0;//current_direction 0:left 1:none 2:right
-	int stop = 0;
+	int stop = 0 ,mstop = 0,max_height = 0 ;
+
 	if (!tree_root){
 	    tree_root = node;
 	    return;
@@ -29,47 +51,74 @@ void insert_avl(struct node * node)
 	
 	next = tree_root;
 	min_no_bf_node = NULL;
+	min_rev_node = NULL;
+
+	if (node->dat < tree_root->dat)
+	    cur_dir = 1;
+	else
+	    cur_dir = 2;
+
 	while(!stop){
 	
 	    if (node->dat < next->dat){//insert it to left
 		
-	  	if(!cur_dir){
-		   cur_dir = 1;
-		}else{
-		   if(!min_rev_node && (cur_dir == 2))
+                if(!min_rev_node && (cur_dir == 2))
 			min_rev_node = next;
 		}
 	    	
-		if (++next->bf >1){
-			min_no_bf_node = next;
-		}
-		
 		if(!next->left){
 		    next->left = node;
 		    node->parent = next;
-
+		    node->maxheight = 0;
 		    stop = 1;
-		}else
+		    //start modify the maxheight of probable parent nodes.
+		    mstop = 0;
+
+		    parent = next->parent;
+		    while(parent && !mstop){
+			parent = next->parent;
+		        parent->bf = get_bf(parent->left,parent->right); 
+			max_height = get_max_height(parent);
+			if(parent->maxheight!=max_height){
+			    parent->maxheight = max_height;
+			    if(parent->bf > 1 || parent->bf<-1)
+				min_no_bf_node = parent;
+			{
+			else
+			    mstop = 1;
+	           }
+
+	}else
 		    next = next->left;	
 
 	    }else{//insert it to right
 
-		if(!cur_dir){
-		   cur_dir = 2;
-		}else{
-		   if(!min_rev_node && (cur_dir == 1))
+	       if(!min_rev_node && (cur_dir == 1))
 			min_rev_node = next;
 		}
 
-		if (--next->bf <-1){
-			min_no_bf_node = next;
-		}
 
 		if(!next->right){
 		    stop = 1 ;
 		    next->right = node;
 		    node->parnet = next;		    
-		   		    
+		    node->maxheight = 0; 		    
+
+		    parent = next->parent;
+		    while(parent && !mstop){
+			parent = next->parent;
+ 			parent->bf = get_bf(parent->left,parent->right);
+			max_height =  get_max_height(parent);
+
+			if(parent->maxheight!=max_height){
+			    parent->maxheight = max_height;
+			    f(parent->bf > 1 || parent->bf<-1)
+			        min_no_bf_node = parent;
+
+			{
+			else
+			    mstop = 1;
+	           }
 		}else	
 		    next = next->right;	
 	    }
@@ -80,13 +129,15 @@ void insert_avl(struct node * node)
 		return;
 	
 	if(cur_dir==1){//left tree no bf
-	    if(!min_rev_node){//need to R reverse
+	    if(!min_rev_node){//need to r reverse
 		if(!min_no_bf_node->parent){//it is root node
 		   tree_root = min_no_bf_node->left;
 		   tree_root->parent=null;
-		   min_no_bf_node->parent = min_no_bf_node->left;
-		   mint_no_bf_node->left=NULL; 	    
-		   
+		   min_no_bf_node->parent = tree_root;
+		   min_no_bf_node->left = tree_root->right;
+		   tree_root->right = min_no_bf_node;		   
+		   tree_root->bf = get_bf(tree_root);
+		    
 		}else{
 		
 		}	
